fed45fb6e33ffbc306147d4b1e7da6672a2a21c5
Lucky A <105838230+jameslucky007@users.noreply.github.com>
2025-12-06 18:48:56 +0530

    commit

--- src/components/LandingPage/Navbar.js
+++ src/components/LandingPage/Navbar.js
@@ -50,7 +50,7 @@ const Navbar = () => {
                 {item}
               </span>
               {/* Smooth underline animation */}
-              <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-gray-800 transition-all duration-300 group-hover:w-full"></span>
+              <span className="absolute left-0 bottom-0 w-0 h-2px bg-gray-800 transition-all duration-300 group-hover:w-full"></span>
             </a>
           ))}
 
