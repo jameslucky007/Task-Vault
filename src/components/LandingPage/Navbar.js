dc26c900a3cd22a253f175f19b3d926e392b7bbc
Lucky A <105838230+jameslucky007@users.noreply.github.com>
2025-12-09 11:59:59 +0530

    commit

--- src/components/LandingPage/Navbar.js
+++ src/components/LandingPage/Navbar.js
@@ -1,8 +1,9 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { ChartNoAxesGantt, X, TrendingUp } from "lucide-react";
+import { ChartNoAxesGantt, X, TrendingUp, ChevronDown} from "lucide-react";
 import { useEffect, useState } from "react";
+
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -34,25 +35,50 @@ const Navbar = () => {
           className="flex gap-2 items-center font-semibold text-gray-900"
         >
           <Image src="/logo.svg" alt="Task Vault logo" width={30} height={30} />
-
           <span className="tracking-wide text-lg flex gap-2">Task Vault</span>
         </Link>
 
         {/* Desktop Nav */}
         <div className="hidden md:flex items-center gap-9 text-gray-800 font-medium">
-          {["Home", "Docs", "Support", "Log In"].map((item) => (
+          {/* Main Nav Links */}
+          {["Home", "Support", "Log In"].map((item) => (
             <a key={item} href="#" className="relative group">
               <span className="cursor-pointer transition-colors duration-200 group-hover:text-gray-900">
                 {item}
               </span>
-              {/* Smooth underline animation */}
-              <span className="absolute left-0 bottom-0 w-0 h-2px bg-gray-800 transition-all duration-300 group-hover:w-full"></span>
+              <span className="absolute left-0 -bottom-2 w-0 h-[2px] bg-gray-800 transition-all duration-300 group-hover:w-full"></span>
             </a>
           ))}
 
-          {/* Sign in / up Buttons */}
+          {/* Dropdown */}
+          <div className="relative group cursor-pointer">
+            <span className="transition-colors duration-200 group-hover:text-gray-900 flex ">
+              Useful Links <ChevronDown size={18} className="mt-1" />
+            </span>
 
-          <button className=" flex gap-3 px-3 py-1.5 text-l font-medium text-white bg-gray-800 rounded-md hover:bg-gray-700 transition">
+            <div
+              className="absolute top-full left-0 mt-2 w-40 bg-white shadow-lg rounded-md opacity-0 scale-95 
+                    group-hover:opacity-100 group-hover:scale-100 transition-all duration-200 z-20"
+            >
+              <ul className="py-2 text-sm text-gray-700">
+                <li className="px-4 py-2 hover:bg-gray-100">
+                  <a href="#">Donate ❤️</a>
+                </li>
+                <li className="px-4 py-2 hover:bg-gray-100">
+                  <a href="#">About Us </a>
+                </li>
+                <li className="px-4 py-2 hover:bg-gray-100">
+                  <a href="#">Documentation</a>
+                </li>
+                <li className="px-4 py-2 hover:bg-gray-100">
+                  <a href="#">GitHub</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Sign Up Button */}
+          <button className="flex gap-3 px-3 py-1.5 text-l font-medium text-white bg-gray-800 rounded-md hover:bg-gray-700 transition">
             Sign up <TrendingUp />
           </button>
         </div>
@@ -70,7 +96,7 @@ const Navbar = () => {
         </button>
       </nav>
 
-      {/* Mobile Menu Overlay */}
+      {/* Mobile Menu */}
       <div
         className={`fixed inset-0 bg-white text-gray-900 flex flex-col items-center justify-center gap-8 z-40 transition-transform duration-500 ${
           menuOpen ? "translate-x-0" : "translate-x-full"
